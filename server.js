ef1613abbbff70366d9bfefdf819ecfa82838867
John Shea <john@johndshea.com>
2015-10-09 10:42:35 -0400

    more stupid syntax errors

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ var express = require(express);
     // required for heroku to work
     PORT    = process.env.PORT || 1337,
     server  = express(),
-    MONGOURI = process.env.MONGOLAB_URI,
+    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
     // change this db name later
     dbname = "project_two",
-    mongoose= require('mongoose');
+    mongoose = require('mongoose');
 
 server.get('/', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 });
 
-mongoose.connect(MONGOURI + / + dbname);
+mongoose.connect(MONGOURI + "/" + dbname);
 server.listen(PORT, function () {
   console.log("Server us up on port: ", PORT);
 });
