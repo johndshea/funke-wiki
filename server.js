fcbc46d9b422fb5958d3e6083a1bd9d04913f369
John Shea <john@johndshea.com>
2015-10-09 10:38:52 -0400

    added mongouri

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require(express);
     // required for heroku to work
-    PORT    = process.env.PORT || 1337;
-    server  = express();
+    PORT    = process.env.PORT || 1337,
+    server  = express(),
+    MONGOURI = process.env.MONGOLAB_URI,
+    dbname = "project_two",
+    mongoose= require('mongoose');
 
-server.get('/', function (req, res, next) {
+server.get('/', function (req, res) {
   res.write("Welcome to my amazing app");
   res.end();
 });
 
+mongoose.connect(MONGOURI + / + dbname);
 server.listen(PORT, function () {
   console.log("Server us up on port: ", PORT);
 });
