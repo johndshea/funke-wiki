962942ca809c9476ab764931915db7d35e0b0239
John Shea <john@johndshea.com>
2015-10-11 21:32:12 -0400

    added materialize theme materials, starting to customize

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 console.log("Client side code loading...");
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
